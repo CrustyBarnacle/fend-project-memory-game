5eb7593bd5ce67fdb48f57b305bb00e6f90f1939
Mario Burgos <mario.burgos@jpl.nasa.gov>
2018-06-25 11:05:58 -0700

    Add ojbect to hold deck, add event handlers for card click.

--- js/app.js
+++ js/app.js
@@ -36,3 +36,11 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+const deck_of_cards = document.querySelectorAll('.card');
+
+for (card of deck_of_cards) {
+  card.addEventListener('click', () => {
+    console.log("Card clicked!");
+  });
+}
